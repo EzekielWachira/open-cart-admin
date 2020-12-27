9f83f93745432cf51c64586e94b157e8a2f9a6d9
Ezekiel Wachira <ezekielwachira048@gmail.com>
2020-12-27 13:17:09 +0300

    :sparkles:add product components

---
+++ src/pages/Phones.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="q-pa-md">
+    <div class="row q-gutter-y-xs">
+        <div class="col-12 col-md-4 col-sm-6 q-gutter-y-xs q-px-xs"
+             v-for="(item, index) in products" :key="index">
+          <item :data="products[index]" @click="logClick" :index="index"/>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import Item from 'components/Item.vue'
+import { mapGetters, mapActions } from 'vuex'
+import { Action, Getter } from 'vuex-class'
+
+@Component({
+  components: {
+    Item
+  },
+  computed: {
+    ...mapActions(['productModule/getAllProducts']),
+    ...mapActions(['productModule/getAllProducts'])
+  }
+})
+export default class Clothes extends Vue {
+  private productItems = []
+  private thumbStyle = {
+    right: '4px',
+    borderRadius: '5px',
+    backgroundColor: '#027be3',
+    width: '5px',
+    opacity: 0.75
+  }
+
+  private barStyle = {
+    right: '2px',
+    borderRadius: '9px',
+    backgroundColor: '#027be3',
+    width: '9px',
+    opacity: 0.2
+  }
+
+  @Getter('productModule/allProducts') products: any
+  @Action('productModule/loggerClick') logClick: any
+  @Action('productModule/getAllProducts') getProducts: any
+  created () {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    this.getProducts()
+    // this.productItems = this.$store.getters['productModule/allProducts']
+  }
+}
+</script>
