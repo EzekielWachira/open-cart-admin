5c3ac744daa9a3def368e03096e92651f80a09f1
Ezekiel Wachira <ezekielwachira048@gmail.com>
2020-12-30 21:38:34 +0300

    :sparkles:add customer page

---
+++ src/pages/Customers.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page>
+    <div class="q-pa-md">
+
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+
+import {Component, Vue} from "vue-property-decorator";
+import customers from "src/utils/Customers";
+
+@Component
+export default class Settings extends Vue {
+  private darkTheme = true
+  private allowBackUp = true
+  private showNotification = false
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/utils/Customers.ts
@@ -0,0 +1,86 @@
+export interface Customer{
+  name: string
+  email: string
+  location: string
+  productsBought: number
+  totalAmountPaid: number
+  balance: number
+  deliveryStatus: string
+}
+
+const customers: Array<Customer> = [
+  {
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    location: 'nairobi',
+    productsBought: 34,
+    totalAmountPaid: 495,
+    balance: 0,
+    deliveryStatus: 'pending'
+  },
+  {
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    location: 'nairobi',
+    productsBought: 34,
+    totalAmountPaid: 495,
+    balance: 0,
+    deliveryStatus: 'pending'
+  },
+  {
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    location: 'nairobi',
+    productsBought: 34,
+    totalAmountPaid: 495,
+    balance: 0,
+    deliveryStatus: 'pending'
+  },
+  {
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    location: 'nairobi',
+    productsBought: 34,
+    totalAmountPaid: 495,
+    balance: 0,
+    deliveryStatus: 'pending'
+  },
+  {
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    location: 'nairobi',
+    productsBought: 34,
+    totalAmountPaid: 495,
+    balance: 0,
+    deliveryStatus: 'pending'
+  },
+  {
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    location: 'nairobi',
+    productsBought: 34,
+    totalAmountPaid: 495,
+    balance: 0,
+    deliveryStatus: 'pending'
+  },
+  {
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    location: 'nairobi',
+    productsBought: 34,
+    totalAmountPaid: 495,
+    balance: 0,
+    deliveryStatus: 'pending'
+  },
+  {
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    location: 'nairobi',
+    productsBought: 34,
+    totalAmountPaid: 495,
+    balance: 0,
+    deliveryStatus: 'pending'
+  }
+]
+
+export default customers
